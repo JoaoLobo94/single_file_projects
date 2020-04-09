ce835250685103177d8e5a5a7fbc9fbaadd02026
Joao Lobo <joao@Joaos-MacBook-Pro.local>
2020-04-09 20:09:24 +0100

    almost  idades

--- idades.rb
+++ idades.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 require 'pry'
-require 'sqlite3'
+require 'set'
 require 'csv'
-require 'parallel'
 
 def idades
   x = 0
@@ -20,14 +19,23 @@ def idades
     r = x * y * z
     menino = (x + y + z) / 2
     padre_age = [x, y, z, menino]
-    @padre_ages = [x, y, z, menino, padre]
-  end
-  @padre_ages
+    all_ages = [x, y, z, menino, padre]
+ end
+    all_ages
 end
 
 def all_combs
   poss_ages = []
-  100000.times { poss_ages.push(idades) unless poss_ages.include?(idades) }
+  100.times { poss_ages.push(idades) }
+  array_of_xyz = []
+  poss_ages.each do |new_age|
+    array_of_xyz << new_age[0]
+    array_of_xyz << new_age[2]
+    array_of_xyz << new_age[1]
+    array_of_xyz << new_age[3]
+  end
+  array_of_xyz.in_groups_of(3){|group| p group}
+
   poss_ages.each do |possible|
     p "As idades possiveis dos passageiros sao #{possible[0]}, #{possible[1]}, #{possible[2]}, sacristao #{possible[3]} e o padre tem #{possible[4]}"
   end
