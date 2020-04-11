b1385f0e78f0ea7fab08b2062fc910623759293e
Joao Lobo <joao@Joaos-MacBook-Pro.local>
2020-04-11 16:28:40 +0100

    idades@

--- idades/idades.rb
+++ idades/idades.rb
@@ -6,12 +6,11 @@ require 'csv'
 
 def idades
   x = 0
-  y = 0
+  y = 
   z = 0
-  padre = 0
   r = x * y * z
   padre_age = []
-  until r == 2450 && padre_age.any? { |age| padre < age }
+  until r == 2450
     x = rand(1..100)
     y = rand(1..100)
     z = rand(1..100)
@@ -19,34 +18,39 @@ def idades
     r = x * y * z
     menino = (x + y + z) / 2
     padre_age = [x, y, z, menino]
-    all_ages = [x, y, z, menino, padre]
  end
-    all_ages
+  padre_age
 end
 
 def all_combs
   poss_ages = []
-  100.times { poss_ages.push(idades) }
-  array_of_xyz = []
-  poss_ages.each do |new_age|
-    array_of_xyz << new_age[0]
-    array_of_xyz << new_age[2]
-    array_of_xyz << new_age[1]
-    array_of_xyz << new_age[3]
+  1000.times { poss_ages.push(idades) }
+  padre_idade = []
+  poss_ages.uniq!
+
+  poss_ages.each do |comb|
+    padre = rand(1..100)
+    padre_idade << padre if comb.any? { |age| padre < age }
   end
-  array_of_xyz.in_groups_of(3){|group| p group}
+  padre_idade
 
   poss_ages.each do |possible|
-    p "As idades possiveis dos passageiros sao #{possible[0]}, #{possible[1]}, #{possible[2]}, sacristao #{possible[3]} e o padre tem #{possible[4]}"
+    p "As idades possiveis dos passageiros sao #{possible[0]}, #{possible[1]}, #{possible[2]}, sacristao #{possible[3]}"
+  end
+    padre_idade.each do |padreco|
+       "O padre pode ter as seguintes idades #{padre_idade}"
   end
   p "Existe, #{poss_ages.count} idades"
+
+
+  p "Assumindo que o sacristao sabe a sua idade"
 
   headers = %w[Pessoa1 Pessoa2 Pessoa3 sacristao padre]
   CSV.open('idade.csv', 'wb', headers: true) do |csv|
     csv << headers
     poss_ages.each do |possible|
-      csv << possible
+       csv << possible
+      end
     end
   end
-end
 all_combs
