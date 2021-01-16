b5ef1b3ea912dfc64d2c0890d21266787ae34201
joao <joao.lobom@gmail.com>
2021-01-16 15:12:04 +0000

    Attempt at hello world asm

---
+++ hello_world.asm
@@ -0,0 +1,18 @@
+; Hello World de joao pedro
+
+global _start
+
+section .text: 
+    mov eax, 0x4
+    mov ebx, 1
+    mov ecx, message
+    mov edx, message_length
+    int 0x80
+
+    mov eax, 0x1
+    mov ebx, 0
+    int 0x80
+
+section .data:
+    message: db "Hello world!", 0xA
+    message_length: equ $-message
